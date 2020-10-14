7fa3d4645653444de5ba5d603c43de23f6bd772f
azkalonz <aeartixenter@gmail.com>
2020-10-14 15:12:28 +0800

    Updates

---
+++ precache-manifest.9b2575102b1207601d86b7679dd63d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee7635aa44abc2f14e1d7e7196c42cd",
+    "url": "/palihug-co-app/index.html"
+  },
+  {
+    "revision": "fa64d2e8c05fb055afac",
+    "url": "/palihug-co-app/static/css/main.97e78cb5.chunk.css"
+  },
+  {
+    "revision": "6926cf4ebcb70e53930d",
+    "url": "/palihug-co-app/static/js/2.8ffabc76.chunk.js"
+  },
+  {
+    "revision": "6d8a6af94c1595ef78e36f3d06c9bbc8",
+    "url": "/palihug-co-app/static/js/2.8ffabc76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa64d2e8c05fb055afac",
+    "url": "/palihug-co-app/static/js/main.8b5a47ad.chunk.js"
+  },
+  {
+    "revision": "e01a2a470193794740ea",
+    "url": "/palihug-co-app/static/js/runtime-main.29c28e80.js"
+  }
+]);
